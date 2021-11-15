ce8183d357fc7ee104b2e8fc1466051a57ad3c7f
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2021-11-15 22:08:50 +1300

    added a few changes

--- views.py
+++ views.py
@@ -5,3 +5,11 @@ views = Blueprint(__name__, 'views')
 @views.route('/')
 def home():
     return render_template("index.html")
+
+@views.route('/generic')
+def get_generic():
+    return render_template("generic.html")
+
+@views.route('/elements')
+def get_elements():
+    return render_template("elements.html")
